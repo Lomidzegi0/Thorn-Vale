5337ec38e79e2c7dee95eed77303bbfff7788083
Lomidzegi0 <lomidzegio81@gmail.com>
2025-03-09 20:03:29 +0400

    Update

--- Thorn Vale/Assets/Scripts/Player/PlayerMovement.cs
+++ Thorn Vale/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,18 +5,23 @@ public class PlayerMovement : MonoBehaviour
     [Header("Config")]
     [SerializeField] private float speed;
 
+    private PlayerAnimations playerAnimations;
     private PlayerActions actions;
     private Rigidbody2D rb2D;
+    private Player player;
+
     private Vector2 moveDirection;
     private void Awake()
     {
+        player = GetComponent<Player>();
         actions = new PlayerActions();
         rb2D = GetComponent<Rigidbody2D>();
+        playerAnimations = GetComponent<PlayerAnimations>();
     }
 
    
 
-    void Update()
+    private void Update()
     {
         ReadMovment();
     }
@@ -28,17 +33,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
+        if (player.Stats.Health <= 0) return;
         rb2D.MovePosition(rb2D.position + moveDirection * (speed * Time.fixedDeltaTime));
+
     }
 
     private void ReadMovment()
     {
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if ( moveDirection == Vector2.zero) 
+        {
+           
+            playerAnimations.SetMoveBoolTransition(false);
+             return;
+        }
+
+         playerAnimations.SetMoveBoolTransition(true);
+         playerAnimations.SetMoveAnimation(moveDirection);
+
     }
 
     private void OnEnable()
     {
-        actions.Enable();
+        
+        actions.Enable();   
     }
     private void OnDisable()
     {
